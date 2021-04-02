f4927f3b4a29d4d2d618fa62be27e1c9ccdb9e19
Guy <guymoustapha@gmail.com>
2021-04-02 14:42:30 +0200

    declare operations and digits in calculate

--- src/logic/calculatate.js
+++ src/logic/calculatate.js
@@ -1,5 +1,10 @@
+import operate from './operate'
+
 export default function calculatate(calculator, buttonName) {
 
   const total, next, operation;
+
+  const operations =  ['+', '-', 'x', '÷', '%'];
+  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 }
