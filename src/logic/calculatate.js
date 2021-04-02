c58237bf32d0d49a20ed14b0f22fa40b9d42bcb5
Guy <guymoustapha@gmail.com>
2021-04-02 15:03:33 +0200

    add logic in calculate

--- src/logic/calculatate.js
+++ src/logic/calculatate.js
@@ -2,9 +2,49 @@ import operate from './operate'
 
 export default function calculatate(calculator, buttonName) {
 
-  const total, next, operation;
+  const {total, next, operation} = calculator ;
 
   const operations =  ['+', '-', 'x', '÷', '%'];
-  const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+  // const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+  if (buttonName === '+/-') {
+    if (next) {
+      next = +next * -1;
+    }
+    total = +total * -1;
+  }
+
+  if (buttonName === '%') {
+    if (next) {
+      next *= 0.01;
+    } else {
+      total *= 0.01;
+    }
+  }
+
+  if (buttonName === '=') {
+    if (next) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = null;
+    }
+  }
+
+  if (operations.includes(buttonName)) {
+    operation = buttonName;
+  }
+
+  if (buttonName === '.') {
+    if (next && !next.toString().contains('.')) {
+      next = `${next.toString()}.`;
+    }
+  }
+
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  }
+
+  return { total, next, operation };
 }
