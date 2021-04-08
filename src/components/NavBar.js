bf8fc11d0c4766af673f2bfad1072df16fd0cb5b
Guy <guymoustapha@gmail.com>
2021-04-08 13:48:41 +0200

    add navbar file

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+    <Link to="/">Home </Link>
+    <Link to="/App">Calculator </Link>
+    <Link to="/Quote">Quote </Link>
+      
+    </div>
+  );
+};
+
+export default NavBar;
